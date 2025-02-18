ed3e0c783f759fc0243c98ddea20dc6bbb5b6513
musniyafidha <musniyafidha5@gmail.com>
2025-02-18 11:46:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const app=express()
+app.get("/",(req,res)=>
+{
+    res.send("<h1>welcome To Ecommerce Website</h1>")
+})
+//http://localhost:4500/search?name=redmi
+app.get("/search",(req,res)=>
+{
+    let prodname=req.query.name
+    res.send(<h1>Details of the product with name ${prodname}</h1>)
+})
+app.listen(4500,()=>console.log("Express server is running on 4500"))
